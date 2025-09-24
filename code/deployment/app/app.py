d9477aff2e8a03f4c63fb4f2044d8072f439c331
spiriton <i.ershov@innopolis.university>
2025-09-24 07:18:54 +0300

    fix: change steps in the streamlit app fields

--- code/deployment/app/app.py
+++ code/deployment/app/app.py
@@ -8,11 +8,11 @@ st.write("Enter patient parameters to predict diabetes probability:")
 
 # Input fields for features
 pregnancies = st.number_input("Pregnancies", min_value=0.0, step=1.0)
-glucose = st.number_input("Glucose", min_value=0.0)
-blood_pressure = st.number_input("Blood Pressure", min_value=0.0)
-skin_thickness = st.number_input("Skin Thickness", min_value=0.0)
-insulin = st.number_input("Insulin", min_value=0.0)
-bmi = st.number_input("BMI", min_value=0.0)
+glucose = st.number_input("Glucose", min_value=0.0, step=1.0)
+blood_pressure = st.number_input("Blood Pressure", min_value=0.0, step=1.0)
+skin_thickness = st.number_input("Skin Thickness", min_value=0.0, step=1.0)
+insulin = st.number_input("Insulin", min_value=0.0, step=1.0)
+bmi = st.number_input("BMI", min_value=0.0, step=0.1)
 diabetes_pedigree_function = st.number_input("Diabetes Pedigree Function", min_value=0.0, step=0.01)
 age = st.number_input("Age", min_value=0.0, step=1.0)
 
